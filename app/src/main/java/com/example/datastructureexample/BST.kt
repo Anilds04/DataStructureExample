3826762e33946e09eff05b95613b1ddac7d1d143
Anil DS <anilds04@gmail.com>
2025-08-08 21:56:38 +0100

    BST programs added

---
+++ app/src/main/java/com/example/datastructureexample/BST.kt
@@ -0,0 +1,90 @@
+package com.example.datastructureexample
+
+fun main() {
+
+    val treeNode = Node(50)
+
+    treeNode.left = Node(40)
+    treeNode.right = Node(70)
+
+    treeNode.left?.left = Node(30)
+    treeNode.left?.right = Node(45)
+
+    treeNode.right?.left = Node(60)
+    treeNode.right?.right = Node(80)
+
+
+
+    println("Output is ${findClosest(treeNode, 35)}")
+}
+
+
+class Node(val value: Int) {
+    var left: Node? = null
+    var right: Node? = null
+}
+
+fun findClosest(
+    node: Node?, target: Int
+): Int? {
+
+    var currentNode = node
+
+    if (currentNode == null)
+        return null
+
+    var closest = currentNode.value
+
+    while (currentNode != null) {
+
+        val value = currentNode.value
+
+        if (Math.abs(value - target) < Math.abs(closest - target)) {
+            closest = value
+        }
+
+        currentNode = if (target < value)
+            currentNode.left
+        else
+            currentNode.right
+
+    }
+
+    return closest
+}
+
+
+fun findIsAvailable(
+    node: Node?, target: Int
+): Boolean {
+
+    var isAvailable = false
+    var currentNode = node
+
+
+    while (currentNode != null) {
+
+        val value = currentNode.value
+
+        if (target == value) {
+            isAvailable = true
+            break
+        }
+
+        val leftDiff = currentNode.left?.value?.let {
+            Math.abs(it - target)
+        }
+
+        val rightDiff = currentNode.right?.value?.let {
+            Math.abs(it - target)
+        }
+
+        currentNode = when {
+            leftDiff != null && (rightDiff != null && leftDiff < rightDiff) -> currentNode.left
+            rightDiff != null -> currentNode.right
+            else -> null
+        }
+    }
+
+    return isAvailable
+}
